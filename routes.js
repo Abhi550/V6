11cde84731f1f854c8472dc5a428ece83ef46e05
ShanGopisetty <125327338+ShanGopisetty@users.noreply.github.com>
2023-05-06 23:40:20 -0400

    Add files via upload

--- routes.js
+++ routes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const userControllers = require('./controllers/user');
 const productControllers = require('./controllers/product');
 const checkAuthMiddleware = require('./middleware/checkAuthMiddleware');
-
+const imageUpload = require('./middleware/imageUpload');
+const checkcart = require('./middleware/checkcart');
 //routes
 
 router.get('/login', userControllers.logIn );
@@ -19,13 +20,26 @@ router.get('/logout' , checkAuthMiddleware , userControllers.logout);
 
 router.get('/dashboard' , checkAuthMiddleware , userControllers.dashboard);
 
-//cart to view cart
-router.get('/cart', checkAuthMiddleware, userControllers.cart);
+//history
+router.get('/history' , checkAuthMiddleware , userControllers.history);
 
+
+
+//cart to view cart
+router.get('/cart', checkAuthMiddleware, checkcart, userControllers.cart);
+
+
+router.post('/updateCart', checkAuthMiddleware , productControllers.updateCart);
 
 //buy and sell
+router.get('/home', checkAuthMiddleware , productControllers.items);
 
+
+
+router.post('/searchByCat', checkAuthMiddleware , productControllers.searchByCat);
 router.post('/addToCart', checkAuthMiddleware , productControllers.addToCart);
+
+
 router.post('/buy', checkAuthMiddleware , productControllers.buy);
 
 router.post('/addProduct', checkAuthMiddleware , productControllers.addProduct);
@@ -34,5 +48,11 @@ router.get('/sell', checkAuthMiddleware , productControllers.sell);
 
 router.post('/updateProduct', checkAuthMiddleware , productControllers.updateProduct);
 router.post('/removeProduct', checkAuthMiddleware , productControllers.removeProduct);
+
+router.post('/request', checkAuthMiddleware , productControllers.requestSeller);
+
+router.post('/acceptRequest', checkAuthMiddleware , productControllers.acceptRequest);
+
+router.get('/notification', checkAuthMiddleware, checkcart , userControllers.notification);
 
 module.exports = router;
